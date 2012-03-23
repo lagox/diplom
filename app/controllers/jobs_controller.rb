eb3d580ec5e2b12c3af056b9699df1354d4cd579
Yerlan Kussainov <lagox.mail@gmail.com>
2012-03-23 13:42:14 +0600

    jobs resource

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,32 @@
+class JobsController < ApplicationController
+  respond_to :html
+  
+  def index
+    @jobs = Job.all
+  end
+  
+  def show
+    
+  end
+  
+  def new
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def create
+    
+  end
+  
+  def update
+    
+  end
+  
+  def destroy
+    
+  end
+  
+end
