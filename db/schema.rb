6795027d37ba742978d31873e6369bbc06632cda
Yerlan Kussainov <lagox.mail@gmail.com>
2012-04-20 11:46:15 +0600

    add originalrating to user(migration)

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-# -*- encoding : utf-8 -*-
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120419112413) do
+ActiveRecord::Schema.define(:version => 20120420054006) do
 
   create_table "categories", :force => true do |t|
     t.string "title"
@@ -53,13 +53,14 @@ ActiveRecord::Schema.define(:version => 20120419112413) do
   create_table "users", :force => true do |t|
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",                                                     :null => false
+    t.datetime "updated_at",                                                     :null => false
     t.string   "name"
     t.string   "avatar"
     t.text     "description"
     t.integer  "rating"
     t.string   "typeuser"
+    t.decimal  "originalrating",  :precision => 6, :scale => 2, :default => 0.0
   end
 
 end
